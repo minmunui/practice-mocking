b4d389929b3a2ab99b147ff6683d7c18d6a382ac
Dongjin <ehdwls1638@gmail.com>
2023-09-22 15:56:29 +0900

    feat : mocking할 mockup 데이터 추가

---
+++ src/mocks/mockingDatas/mockData.js
@@ -0,0 +1,16 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        title: 'Test Product 1',
+        price: 100,
+        description: 'This is a test product',
+        image: '/images/airpods.jpg'
+    },
+    {
+        id: 2,
+        title: 'Test Product 2',
+        price: 200,
+        description: 'This is a test product',
+        image: '/images/airpods.jpg'
+    }
+];
